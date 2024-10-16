7945bc40dce65574988f6c1566b45bfd9e92c6c2
MichailBolsun32 <bolsun32@mail.ru>
2024-10-17 01:49:16 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,16 @@
+#Напишите функцию-генератор all_variants(text),
+# которая принимает строку text и возвращает объект-генератор,
+# при каждой итерации которого будет возвращаться подпоследовательности переданной строки.
+
+
+def all_variants(text):
+    for length in range(1, len(text) + 1):  # итерация по всей строке
+        for index_start in range(len(text) - length + 1):  # итерация по длине подстроки
+           yield text[index_start:index_start + length]  # Возвращаем подстроку
+
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
